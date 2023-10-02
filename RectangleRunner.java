9a9a2574c1b65a24f80fd5d79d0e491bcde463d7
newman-csa <logann24@nycstudents.net>
2023-10-02 06:58:57 -0400

    Initial Commit

---
+++ RectangleRunner.java
@@ -0,0 +1,22 @@
+public class RectangleRunner {
+    public static void main(String[] args) {
+        // Rectangle rect1 = new Rectangle(150, 200);
+        // Rectangle rect2 = new Rectangle(125);
+        // Rectangle rect3 = new Rectangle();
+
+        // rect1.setWidth(75);
+        // rect2.setWidth(75);
+        // rect2.setLength(75);
+        // rect3.setWidth(75);
+
+        // System.out.println("These three plots requires "+ (rect1.calculateArea() + rect2.calculateArea()+  rect3.calculateArea()) +" square feet of seed");
+
+        int len = 65;
+        int wid = len + 10;
+        Rectangle rect1 = new Rectangle(len, wid);
+        Rectangle rect2 = new Rectangle(10.0, 30.0);
+        Rectangle rect3 = new Rectangle(0);
+
+
+    }
+}
